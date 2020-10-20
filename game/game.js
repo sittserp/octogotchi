c47ce4f1411b045423bd2aac16bdb8e5afa8a45e
DonnyLVu <donnyvu897@gmail.com>
2020-10-20 16:23:15 -0700

    add working food and health button

--- game/game.js
+++ game/game.js
@@ -1,5 +1,11 @@
 import { setOctopus, getOctopus, noHp, OCTOPUS } from '../utils.js';
 
+const rArrow = document.getElementById('right-arrow');
+const lArrow = document.getElementById('left-arrow');
+const mArrow = document.getElementById('middle-arrow');
+
+
+// Function stuff
 function eatFood() {
     const octopus = getOctopus();
     if (octopus.food > 0) {
@@ -15,14 +21,36 @@ function loseHealth() {
     }
     setOctopus(octopus);
 }
+function addFood() {
+    const octopus = getOctopus();
+    if (octopus.food < 4) {
+        octopus.food++;
+    }
+    setOctopus(octopus);
+}
+function addHealth() {
+    const octopus = getOctopus();
+    if (octopus.hp < 6) {
+        octopus.hp++;
+    }
+    setOctopus(octopus);
+}
 
-const hungry = setInterval(() => { eatFood(); }, 1000);
 
-const starving = setInterval(() => { loseHealth(); }, 1000);
+const hungry = setInterval(() => { eatFood(); }, 5000);
+
+const starving = setInterval(() => { loseHealth(); }, 5000);
 
 hungry;
 starving;
 
+// Arrows 
+
+rArrow.addEventListener('click', () => {
+    addFood();
+    addHealth();
+    
+});
 
 
 
