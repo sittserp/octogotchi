b575469482af360568e294b8bfb8206bf1aee611
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-10-20 16:36:15 -0700

    cleaned

--- game/game.js
+++ game/game.js
@@ -1,11 +1,13 @@
 import { setOctopus, getOctopus, noHp, OCTOPUS } from '../utils.js';
 
+
+// Set const----------------------------------------------------------------------------------------------------------------------------
 const rArrow = document.getElementById('right-arrow');
 const lArrow = document.getElementById('left-arrow');
 const mArrow = document.getElementById('middle-arrow');
 
 
-// Function stuff
+// Function stuff ---------------------------------------------------------------------------------------------------------------------
 function eatFood() {
     const octopus = getOctopus();
     if (octopus.food > 0) {
@@ -13,7 +15,6 @@ function eatFood() {
     }
     setOctopus(octopus);
 }
-
 function loseHealth() {
     const octopus = getOctopus();
     if (octopus.food === 0 && octopus.hp > 0) {
@@ -36,52 +37,48 @@ function addHealth() {
     setOctopus(octopus);
 }
 
+// -------------------------------------------------------------------------------------------------------------------------------------
 
-<<<<<<< dev-donny
-const hungry = setInterval(() => { eatFood(); }, 5000);
-=======
+// gets Octopus from local storage
 const octopus = getOctopus();
+
 // const currentOctopus = setInterval(() => { getOctopus(); }, 1000);
-
+// octopus eats food every 1 second
 const hungry = setInterval(() => { eatFood(); }, 1000);
->>>>>>> main
 
-const starving = setInterval(() => { loseHealth(); }, 5000);
+// octopus loses health every 1 second
+const starving = setInterval(() => { loseHealth(); }, 1000);
 
 // const octoStatus = setInterval(() => { console.log(currentOctopus.hp, 'octoStatus'); }, 1000);
-
+// checks hp of octopus on timed interval
 const hpCheck = setInterval(() => { if (noHp(octopus)) { window.location.href = '../index.html'; } }, 1000);
 
 
 hungry;
 starving;
-// currentOctopus;
-// octoStatus;
-
-
-// setInterval(() => { console.log(noHp(octopus), 'noHp'); }, 1000);
 
 hpCheck;
-
 console.log(hpCheck);
 
+
+// currentOctopus;
+// octoStatus;
+// setInterval(() => { console.log(noHp(octopus), 'noHp'); }, 1000);
 // const hpCheck = setInterval(() => {
 //     if (octopus.hp === 0) {
 //         window.location.href = '../index.html';
 //     }
 // }, 1000);
 
-<<<<<<< dev-donny
-// Arrows 
+// Event Listerners for arrows ---------------------------------------------------------------------------------------------------------------------
 
 rArrow.addEventListener('click', () => {
     addFood();
     addHealth();
-    
+
 });
-=======
-// hpCheck;
->>>>>>> main
+
+
 
 
 
