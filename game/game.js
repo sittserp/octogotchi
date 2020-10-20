9e7908490afd5d7dcef49681bd0a4f03e6ec42f5
Perry Sittser <sittserp@gmail.com>
2020-10-20 16:19:13 -0700

    working on hpCheck

--- game/game.js
+++ game/game.js
@@ -16,13 +16,37 @@ function loseHealth() {
     setOctopus(octopus);
 }
 
+const octopus = getOctopus();
+// const currentOctopus = setInterval(() => { getOctopus(); }, 1000);
+
 const hungry = setInterval(() => { eatFood(); }, 1000);
 
 const starving = setInterval(() => { loseHealth(); }, 1000);
 
+// const octoStatus = setInterval(() => { console.log(currentOctopus.hp, 'octoStatus'); }, 1000);
+
+const hpCheck = setInterval(() => { if (noHp(octopus)) { window.location.href = '../index.html'; } }, 1000);
+
+
 hungry;
 starving;
+// currentOctopus;
+// octoStatus;
 
+
+// setInterval(() => { console.log(noHp(octopus), 'noHp'); }, 1000);
+
+hpCheck;
+
+console.log(hpCheck);
+
+// const hpCheck = setInterval(() => {
+//     if (octopus.hp === 0) {
+//         window.location.href = '../index.html';
+//     }
+// }, 1000);
+
+// hpCheck;
 
 
 
